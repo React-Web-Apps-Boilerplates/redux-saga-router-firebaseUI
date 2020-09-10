9dd5328b43f31240b3f9319068dd6f1ceea0fddd
Flavin John <Flavinj@WinjitTechnologiesPvtLtd.local>
2020-09-10 10:27:37 +0530

    Firebase config and utility added

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,41 +1,16 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-
-firebase.initializeApp({
-    apiKey: "AIzaSyDLoqcbTDMFuurtAyDgVEKZ6qwo0j0Osjk",
-    authDomain: "fir-auth-tutorial-ed11f.firebaseapp.com",
-});
+import { firebaseAuth, getCurrentUser, signOut } from "../../utility/firebase";
+import {uiConfig} from "../../settings/firebase/uiConfig"
 
 class App extends Component {
     state = { isSignedIn: false };
-    uiConfig = {
-        signInFlow: "popup",
-        signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-            firebase.auth.GithubAuthProvider.PROVIDER_ID,
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            {
-                provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-                recaptchaParameters: {
-                    type: "image", // 'image or audio'
-                    size: "normal", // 'normal or invisible' or 'compact'
-                    badge: "bottomleft", // 'bottomLeft' or 'bottomright' or 'inline' applies to invisible.
-                },
-                defaultCountry: "IN",
-            },
-        ],
-        callbacks: {
-            signInSuccess: () => false,
-        },
-    };
 
     componentDidMount = () => {
-        firebase.auth().onAuthStateChanged((user) => {
+        firebaseAuth.onAuthStateChanged((user) => {
             this.setState({ isSignedIn: !!user });
-            console.log("user", user, firebase.auth().currentUser);
+            console.log("user", user, getCurrentUser());
         });
     };
 
@@ -45,21 +20,21 @@ class App extends Component {
                 {this.state.isSignedIn ? (
                     <span>
                         <div>Signed In!</div>
-                        <button onClick={() => firebase.auth().signOut()}>
+                        <button onClick={() => signOut()}>
                             Sign out!
                         </button>
                         <h1>
-                            Welcome {firebase.auth().currentUser.displayName}
+                            Welcome {getCurrentUser().displayName}
                         </h1>
                         <img
                             alt="profile pic"
-                            src={firebase.auth().currentUser.photoURL}
+                            src={getCurrentUser().photoURL}
                         />
                     </span>
                 ) : (
                     <StyledFirebaseAuth
-                        uiConfig={this.uiConfig}
-                        firebaseAuth={firebase.auth()}
+                        uiConfig={uiConfig}
+                        firebaseAuth={firebaseAuth}
                     />
                 )}
             </div>
