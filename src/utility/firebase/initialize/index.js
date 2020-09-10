3f0c643023e269cd171f1626fb33860ea6420f28
Flavin John <Flavinj@WinjitTechnologiesPvtLtd.local>
2020-09-10 22:44:19 +0530

    Call dependent functions on initialization after initialization in same file itself

---
+++ src/utility/firebase/initialize/index.js
@@ -0,0 +1,12 @@
+import { initializeApp, auth, firestore } from "firebase";
+import { config as firebaseConfig } from "../../../settings/firebase/config";
+
+/* Initialize default App. 
+    - Now importing "firebase" in other files will be this initialized default App.
+    - Can initialize multiple Apps also. For that, initialize and store it to a constant.
+        This constant can be used to access the specific app.*/
+initializeApp(firebaseConfig)
+
+/* Functions immediately dependent on initialization */
+export const firebaseAuth = auth();
+// export const database = firestore();
